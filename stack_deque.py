b6b4619bc1aaaf77e0d4fcd4cae702b5b6ab05bb
ABINASH A. <allesabinash1@gmail.com>
2024-08-03 23:04:54 +0530

    Add Stack

---
+++ stack_deque.py
@@ -0,0 +1,10 @@
+from collections import deque
+
+stack = deque()
+stack.append("https://www.cnn.com/")
+stack.append("https://www.cnn.com/world")
+stack.append("https://www.cnn.com/india")
+stack.append("https://www.cnn.com/china")
+print(stack)
+print(stack.pop())
+print(stack)
